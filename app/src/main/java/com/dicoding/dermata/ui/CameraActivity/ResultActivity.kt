acd2c1547f4891b0f2aae5bb672453266e02f61f
putra1505 <rangga5mei2001@gmail.com>
2024-12-13 10:02:15 +0700

    bottom navigation

--- app/src/main/java/com/dicoding/dermata/ui/CameraActivity/ResultActivity.kt
+++ app/src/main/java/com/dicoding/dermata/ui/CameraActivity/ResultActivity.kt
@@ -1,11 +1,15 @@
 package com.dicoding.dermata.ui.CameraActivity
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.dicoding.dermata.R
 import com.dicoding.dermata.databinding.ActivityResultBinding
+import com.dicoding.dermata.ui.ComingSoonPage.ComingSoonActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
@@ -14,6 +18,7 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupBottomNavigation()
 
         // Ambil data dari Intent
         val prediction = intent.getStringExtra(EXTRA_PREDICTION)
@@ -61,5 +66,36 @@ class ResultActivity : AppCompatActivity() {
         const val EXTRA_PREDICTION = "extra_prediction"
         const val EXTRA_CONFIDENCE = "extra_confidence"
         const val EXTRA_IMAGE_URI = "extra_image_uri"
+    }
+    private fun setupBottomNavigation() {
+        Log.d("MainActivity", "Setting up bottom navigation")
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            Log.d("MainActivity", "Menu item selected: ${menuItem.itemId}")  // Log saat menu item dipilih
+            when (menuItem.itemId) {
+                R.id.menu_camera -> {
+
+                    Log.d("MainActivity", "Camera button clicked, redirecting to AnalysisActivity")
+                    startActivity(Intent(this, AnalysisActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+                R.id.menu_profile, R.id.menu_aichat -> {
+                    Log.d("MainActivity", "Redirecting to ComingSoonActivity")  // Log saat membuka ComingSoonActivity
+                    startActivity(Intent(this, ComingSoonActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+                R.id.menu_aichat -> {
+                    Log.d("MainActivity", "Redirecting to ComingSoonActivity")  // Log saat membuka ComingSoonActivity
+                    startActivity(Intent(this, ComingSoonActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+                else -> false
+            }
+        }
+        bottomNavigationView.selectedItemId = R.id.menu_camera
+        Log.d("MainActivity", "Bottom navigation set to 'main'")  // Log saat bottom navigation diset
     }
 }
